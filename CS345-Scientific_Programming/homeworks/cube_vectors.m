68e3e1757dcbf633de3620eb4aef377f9f210b97
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2021-02-18 17:54:51 -0500

    Added cubic plot assignment

--- CS345-Scientific_Programming/homeworks/cube_vectors.m
+++
@@ -1,5 +0,0 @@
-u = randi(10, 100,1);
-v = 1:length(u);
-for i = 1:length(u)
-  v(i) = u(i) ^ 3;
-end
--- CS345-Scientific_Programming/homeworks/plot_cubed_numbers.m
+++
@@ -1,5 +0,0 @@
-function plot_cubed_numbers
-    x = 0:0.01:10;
-    y = x.^3;
-    plot(x,y), xlabel('x'), ylabel('cube(x)'), title('Cubes of x values');
-end
